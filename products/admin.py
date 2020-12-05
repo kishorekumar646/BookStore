cae0234caed9cd4731891b89c6e979fe4b4a05e2
kishorekumar646 <kishorekumar131646@gmail.com>
2020-12-05 16:39:51 +0530

    [kishore]Refactor. custmize admin page

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+from .models import Book
 
-# Register your models here.
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (_('Book info'), {'fields': (
+            ('book_title', 'book_author'), 'book_description', 'book_category')}),
+        (_('Important dates'), {'fields': ('created_at', 'modified_at')}),
+    )
+    list_display = ('book_title','book_author','created_at')
+    list_filter = ('created_at', 'modified_at')
+    search_fields = ('book_title','book_author')
+    readonly_fields = ('created_at', 'modified_at',)
+    list_per_page = 10
--- products/models.py
+++ products/models.py
@@ -1,6 +1,25 @@
 from django.db import models
+from bookstore_backend.validators import *
+
+CATEGORY = (
+    (1, "Programming"),
+    (2, "Networking"),
+    (3, "Python")
+)
 
 
 class Book(models.Model):
-    book_title = models.CharField(max_length = 150)
+    book_title = models.CharField(
+        max_length=255, validators=[BookNameValidator],)
+    book_author = models.CharField(
+        max_length=255, validators=[BookAuthorValidator],)
+    book_description = models.CharField(max_length=255, validators=[
+                                        BookNameValidator], null=True, blank=True)
+    book_category = models.PositiveSmallIntegerField(
+        choices=CATEGORY, default=None, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
     
+
+    def __str__(self):
+        return '%s - %s' % (self.book_title, self.book_author)
