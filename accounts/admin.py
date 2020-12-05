3b281a2409f2c7d5039b3ab0be4b931cd9d74bc1
kishorekumar646 <kishorekumar131646@gmail.com>
2020-12-05 13:47:46 +0530

    [kishore]Refactor. display of user in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from .models import User, UserDocument, UserWithName
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import ugettext_lazy as _
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    """Define admin model for custom User model with no email field."""
+
+    fieldsets = (
+        (_('Personal info'), {'fields': (('user_photo', 'profile_photo',), ('first_name', 'last_name',),
+                                         'phone_number', 'email',
+                                         'password')}),
+        (_('Permissions'), {'fields': ('user_type', 'is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('phone_number', 'email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('phone_number', 'email', 'first_name',
+                    'last_name', 'date_joined', 'imei_no', 'is_superuser')
+    list_filter = ('is_superuser', 'is_staff', 'is_active')
+    search_fields = ('phone_number', 'email', 'first_name', 'last_name')
+    readonly_fields = ('profile_photo', 'imei_no',)
+    ordering = ('date_joined',)
+    list_per_page = 10
+
+
+@admin.register(UserDocument)
+class UserDocumentAdmin(admin.ModelAdmin):
+    model = UserDocument
+    fields = ('user_document_type', 'user_document_number',
+              'user_document_photo', 'user_document_photo_thumbnail', )
+    readonly_fields = ('user_document_photo_thumbnail',)
+
+
+@admin.register(UserWithName)
+class UserWithNameAdmin(admin.ModelAdmin):
+    model = UserWithName
+    search_fields = ('phone_number', 'email', 'first_name', 'last_name')
+
+
+admin.site.unregister(Group)
--- bookstore_backend/settings.py
+++ bookstore_backend/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     # apps #
 
     'accounts',
+    'products',
 
 
 ]
