eaf5ae816c57b8f9b7f76b40b089766ce4e2d283
kishorekumar646 <kishorekumar131646@gmail.com>
2020-12-10 15:43:42 +0530

    [kishore]Add. created urls.py for accounts app

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url, include
+from django.urls import path
+from .views import login,register,logout
+
+urlpatterns = [
+    path('login/', login, name='login'),
+    path('register/', register, name='register'),
+    path('logout/', logout, name='logout'),
+]
