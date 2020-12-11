138111e59ea58ebd0ea593d01b028c88b789f95b
kishorekumar646 <kishorekumar131646@gmail.com>
2020-12-11 10:47:49 +0530

    [kishore]Add. forms.py file into accounts app

---
+++ accounts/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.core.validators import RegexValidator
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class LoginForm(forms.ModelForm):
+    
+    class Meta:
+        model = User
+        fields = ['phone_number','password']
