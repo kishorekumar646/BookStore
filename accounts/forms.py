a20b45d79d7969caeb4eb69fc02a48fdd33efef6
kishorekumar646 <kishorekumar131646@gmail.com>
2020-12-11 12:54:41 +0530

    [kishore]Refactor. LoginForm() changed

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,18 +5,13 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-class LoginForm(forms.ModelForm):
+class LoginForm(forms.Form):
 
-    class Meta:
-        model = User
-        fields = ['phone_number', 'password']
-        widgets = {
-            'password': forms.PasswordInput(attrs={'placeholder': 'Enter a password', 'class': 'mb-4'}),
-            'phone_number': forms.TextInput(attrs={'placeholder': 'Enter a valid phone number', 'class': 'mb-4'}),
-        }
+    phone_number = forms.CharField(max_length=10,widget=forms.TextInput(attrs={'placeholder': 'Enter a valid phone number', 'class': 'mb-4'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Enter a password', 'class': 'mb-4'}))
 
     def clean(self):
-        super(LoginForm,self).clean()
+        super(LoginForm, self).clean()
         print("Clean")
 
         phone_number = self.cleaned_data.get('phone_number')
@@ -24,3 +19,15 @@ class LoginForm(forms.ModelForm):
         print(phone_number)
 
         return self.cleaned_data
+
+
+class RegisterForm(forms.ModelForm):
+
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Enter a confirm password'}))
+    class Meta:
+        model = User
+        fields = ['phone_number','password','email']
+        widgets = {
+            'password': forms.PasswordInput(attrs={'placeholder': 'Enter a password', 'class': 'mb-4'}),
+            'phone_number': forms.TextInput(attrs={'placeholder': 'Enter a valid phone number', 'class': 'mb-4'}),
+        }
--- accounts/views.py
+++ accounts/views.py
@@ -1,31 +1,44 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.views import View
-from .forms import LoginForm
+from .forms import LoginForm, RegisterForm
 
 
 class Login(View):
 
     def get(self, request):
         form = LoginForm()
-        return render(request, "accounts/login.html",{'form': form})
+        return render(request, "accounts/login.html", {'form': form})
 
     def post(self, request):
-        
+
         form = LoginForm(request.POST)
+        print(form.errors)
         if form.is_valid():
             print(request.POST)
 
             return redirect('home')
         else:
-            return render(request, "accounts/login.html",{'form': form})
+            return render(request, "accounts/login.html", {'form': form})
 
 
 class Register(View):
 
     def get(self, request):
+        form = RegisterForm()
+        return render(request, "accounts/register.html",{'form': form})
 
-        return render(request, "accounts/register.html")
+    def post(self, request):
+
+        form = RegisterForm(request.POST)
+        print(form.errors)
+        print(form.is_valid())
+        if form.is_valid():
+            print(request.POST)
+
+            return redirect('login')
+        else:
+            return render(request, "accounts/register.html", {'form': form})
 
 
 class Logout(View):
